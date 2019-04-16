e51d3833780498e01aabdd67c53ac1a595e53ea1
Yiyi Sun <yiyisun@gmail.com>
2019-04-16 10:38:14 -0400

    add toolbar to the tool window

--- VSIXProject2019/GitChangesWindow.cs
+++ VSIXProject2019/GitChangesWindow.cs
@@ -1,6 +1,7 @@
 ﻿namespace VSIXProject2019
 {
     using System;
+    using System.ComponentModel.Design;
     using System.Runtime.InteropServices;
     using Microsoft.VisualStudio.Shell;
 
@@ -27,7 +28,7 @@
         public GitChangesWindow(GitChangesWindowState state) : base()
         {
             this.Caption = "Git Changes";
-
+            this.ToolBar = new CommandID(GuidList.guidVsGitToolsPackageCmdSet, PkgCmdIDList.imnuGitChangesToolWindowToolbarMenu);
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
