dd7861a9582aa619ad7dc82b1fe753b3ca0f34cb
Yiyi Sun <yiyisun@gmail.com>
2019-04-17 10:28:35 -0400

    add git tracker

---
+++ VSIXProject2019/GitTracker.cs
@@ -0,0 +1,28 @@
+﻿using GitScc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSIXProject2019
+{
+    public delegate void ChangedEventHandler(GitTracker tracker);
+
+    public class GitTracker
+    {
+        public string Directory { get; }
+        public GitRepository Repository { get; }
+
+        public event ChangedEventHandler Changed;
+
+        public GitTracker(string directory)
+        {
+            this.Directory = directory;
+            this.Repository = new GitRepository(directory);
+        }
+
+
+
+    }
+}
